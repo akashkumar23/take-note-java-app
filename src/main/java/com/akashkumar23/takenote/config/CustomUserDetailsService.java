21d053c154c3b8e0a89fc98155b89e05cd555519
Akash Kumar <77935092+akashkumar23@users.noreply.github.com>
2025-03-06 17:56:48 +0530

    Implement custom UserDetailsService for authentication

---
+++ src/main/java/com/akashkumar23/takenote/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.akashkumar23.takenote.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.akashkumar23.takenote.entity.User;
+import com.akashkumar23.takenote.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = this.userRepository.findByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("User not found!");
+		}else {
+			return new CustomUser(user);//custom user class constructor.
+		}
+		
+		
+	}
+
+}
